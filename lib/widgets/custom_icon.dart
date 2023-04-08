ca9022d623bf53798eab23181874b9c2a84b950b
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-04-08 17:22:52 +0200

    app bar of notes view

---
+++ lib/widgets/custom_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.09),
+          borderRadius: BorderRadius.circular(16)),
+      child: const Icon(
+        Icons.search_rounded,
+        size: 27,
+      ),
+    );
+  }
+}
