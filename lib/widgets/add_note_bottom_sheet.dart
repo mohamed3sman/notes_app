7b1324aa3193a91541ed9ee2c29fe6bc20344116
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-04-09 17:24:03 +0200

    create title text field for bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 35,
+          ),
+          CustomTextField()
+        ],
+      ),
+    );
   }
 }
